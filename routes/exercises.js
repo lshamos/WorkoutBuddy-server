6fa4653eaf74519a64748aa9c5ddb3d38112be3b
Lorin Shamos <lshamos@gmail.com>
2017-08-08 16:29:22 -0700

    update routes

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const exercisesRouter = new express.Router()
-
+const Exercise = require('../models/Exercise')
 exercisesRouter.get('/', (req, res) => {
   Exercise.find({_user: req.user._id},
     (err, exercises) => {
-    res.json({message: 'exercises root'})
+    res.json(exercises)
   })
+})
+
+exercisesRouter.post('/', (req, res) => {
+  const newExercise = new Exercise(req.body)
+  newExercise._user = req.user._id
+  newExercise.save((err, exercise) => {
+    res.json({message: 'exercise created', success: true, exercise})
+  })
+
 })
 
 
